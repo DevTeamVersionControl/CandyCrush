d3e48cabb5a6e496e9b2eddd13e74b84db44bd12
kcnq <2135433@cegeplimoilou.ca>
2021-12-02 10:54:51 -0500

    Starting structure of Icing Gun

--- IcingGun/Scripts/Player.gd
+++ IcingGun/Scripts/Player.gd
@@ -2,29 +2,37 @@
 extends KinematicBody2D
 
 const MAX_FALL_SPEED = 120.5
-const MAX_SPEED = 7
+const DEFAULT_MAX_SPEED = 7
 const JUMP_FORCE = 30
 const ACCEL = 1
 const MAX_BULLET_STRENGTH = 1
 
+var max_speed = DEFAULT_MAX_SPEED
 var facing = Vector2(1,0)
 var bullet_direction = Vector2()
 var bullet_strength = 0
 var hp := 10
-var equiped_ammo
+var popping_candy
+var icing_gun = preload("res://Scenes/IcingGun.tscn")
+var equiped_ammo = icing_gun
 var can_shoot := true
 var invulnerable := false
+var bullet
 
 var motion = Vector2()
 
 func _physics_process(delta):
-	
 	if Input.is_action_just_pressed("quit_game"):
 		get_tree().quit()
 	
 	if(Input.is_action_just_pressed("shoot")):
-		if can_shoot && equiped_ammo != null:
-			shoot(equiped_ammo, equiped_ammo.strength)
+		if can_shoot && equiped_ammo != null && !bullet:
+			shoot(equiped_ammo)
+		elif can_shoot && equiped_ammo != null && (equiped_ammo.is_in_group("locked")):
+			if bullet.rotation_degrees == -90 && facing.x != 1:
+				shoot(equiped_ammo)
+			elif bullet.rotation_degrees == 90 && facing.x != -1:
+				shoot(equiped_ammo)
 	
 	motion.y += get_parent().GRAVITY/2 * pow(delta * 45,2)
 	
@@ -39,15 +47,31 @@ func _physics_process(delta):
 		motion.y = MAX_FALL_SPEED
 	
 	if (Input.is_action_pressed("right")):
-		motion.x += ACCEL
-		facing.x = 1
+		if bullet:
+			if bullet.locked == true:
+				motion.x += ACCEL
+				max_speed = bullet.get_player_max_speed(DEFAULT_MAX_SPEED)
+			else:
+				motion.x += ACCEL
+				facing.x = 1
+		else:
+			motion.x += ACCEL
+			facing.x = 1
 	elif (Input.is_action_pressed("left")):
-		motion.x -= ACCEL
-		facing.x = -1
+		if bullet:
+			if bullet.locked == true:
+				motion.x -= ACCEL
+				max_speed = bullet.get_player_max_speed(DEFAULT_MAX_SPEED)
+			else:
+				max_speed = DEFAULT_MAX_SPEED
+				motion.x -= ACCEL
+				facing.x = -1
+		else:
+			motion.x -= ACCEL
+			facing.x = -1
 	else:
 		motion.x = lerp(motion.x, 0, 0.2)
-	
-	motion.x = clamp(motion.x, -MAX_SPEED, MAX_SPEED)
+	motion.x = clamp(motion.x, -max_speed, max_speed)
 	
 	if is_on_floor():
 		if (Input.is_action_just_pressed("jump")):
@@ -64,12 +88,14 @@ func _physics_process(delta):
 	if global_position.y > get_parent().screen_size.y:
 		get_tree().reload_current_scene()
 
-func shoot(ammo, strength):
+func shoot(ammo):
+	if ammo == popping_candy:
+		if bullet:
+			bullet.queue_free()
 	if can_shoot:
-		var bullet = ammo.instance()
-		get_tree().current_scene.add_child(bullet)
+		bullet = ammo.instance()
+		add_child(bullet)
 		bullet.global_transform = global_transform
-		bullet_direction = bullet_direction.normalized() * bullet_strength
 		bullet.launch(bullet_direction)
 		can_shoot = false
 		$AmmoTimer.start(bullet.COOLDOWN)
@@ -89,7 +115,6 @@ func take_damage(damage, direction):
 func _on_AmmoTimer_timeout():
 	can_shoot = true
 
-
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("destructable"):
 		body.can_reappear = false
@@ -97,13 +122,11 @@ func _on_Area2D_body_entered(body):
 		if !invulnerable:
 			take_damage(2, body.motion)
 
-
 func _on_Area2D_body_exited(body):
 	if body.is_in_group("destructable"):
 		body.can_reappear = true
 		if body.should_reappear:
 			body.reappear()
-
 
 func _on_InvulnerabilityTimer_timeout():
 	invulnerable = false
